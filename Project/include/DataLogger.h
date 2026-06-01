1e6fe9df9b189836ab035d78b8f46a627ee52450
IhorHuz <fryfat114@gmail.com>
2026-06-01 18:52:53 +0200

    GREEN: Implement DataLogger skeleton to pass empty test. next RED: Add failing test for adding a reading to DataLogger

---
+++ Project/include/DataLogger.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class DataLogger
+{
+public:
+    int getLogCount()
+    {
+        return 0;
+    }
+};
--- Project/tests/DataLoggerTest.cpp
+++ Project/tests/DataLoggerTest.cpp
@@ -6,3 +6,10 @@ TEST(DataLoggerTest, NewLoggerIsEmpty)
     DataLogger logger;
     EXPECT_EQ(0, logger.getLogCount());
 }
+
+TEST(DataLoggerTest, AddingReadingIncreasesCount)
+{
+    DataLogger logger;
+    logger.addReading(22.5);
+    EXPECT_EQ(1, logger.getLogCount());
+}
